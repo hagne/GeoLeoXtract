67c62e7edbfb50093a56b6541384b65287627ca9
Hagen Telg <hagen@hagnet.net>
2021-04-22 20:58:58 -0600

    Update __init__.py
    added some code to address the error produced by basemap when imported from within a conda environment.

--- nesdis_gml_synergy/__init__.py
+++ nesdis_gml_synergy/__init__.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
 
+
+# on anaconda importing basemap can cause errors which can be fixed by setting
+# the following variable
+try:
+    import mpl_toolkits.basemap as __
+except KeyError as er:
+    if er.args[0] == 'PROJ_LIB':
+        import os as _os
+        _os.environ['PROJ_LIB']  = _os.environ['CONDA_PREFIX']
+        import mpl_toolkits.basemap as __
+    else:
+        raise
